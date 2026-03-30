b58bff805b6c1273907b18000afde85524d3f23d
Faysal <faysalalshariar@gmail.com>
2026-03-30 09:26:14 +0600

    False Position Method implementation for root finding
    This implementation uses the False Position Method to find the root of the function f(x) = x - e^-x within the interval [0, 1] over 30 iterations. It outputs the approximate root and the number of iterations taken.

---
+++ False Position Method 3.m
@@ -0,0 +1,20 @@
+f = @ (x) x - e^-x 
+a = 0;
+b = 1;
+n = 30;
+
+if f(a)*f(b) < 0
+    for i = 1:n
+        c = (a*f(b)-b*f(a))/(f(b)-f(a));
+        if f(a)*f(c) < 0
+            b = c;
+        elseif f(b)*f(c) < 0
+            a = c;
+        end
+    end
+    fprintf('The approximate root of the function is: %.7f\n', c);
+    fprintf('Number of iterations: %d', i);
+
+else 
+    disp('There is no root of this function, try using different intervals.');
+end
